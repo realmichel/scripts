4aba79dd8890cae4dd9029b68cbb270e0abc5b9c
realmichel <asthelightstartstodim@hotmail.com>
2023-06-02 00:11:46 -0300

    Add files via upload

---
+++ yay.sh
@@ -0,0 +1,2 @@
+git clone https://aur.archlinux.org/yay-bin.git; cd yay-bin; makepkg -si;
+yay -S geekbench nomacs spotify-adblock nsnake dupeguru-git ventoy gcfscape losslesscut-bin path-of-building-community-git  android-sdk-platform-tools vtfedit balena-etcher openmw-git deemix-git jdownloader2 megasync-bin duckstation-git
